c08e80b60395aa738eaa8f8eb411ca7004ffac9d
colbyfayock <fayock@gmail.com>
2020-10-18 11:45:35 -0400

    Step 3: adding responsive styles to grid cards

--- pages/index.js
+++ pages/index.js
@@ -21,27 +21,27 @@ export default function Home() {
         </Text>
 
         <Flex flexWrap="wrap" alignItems="center" justifyContent="center" maxW="800px" mt="10">
-          <Box as="a" href="https://nextjs.org/docs" p="6" m="4" borderWidth="1px" rounded="lg" flexBasis="45%">
+          <Box as="a" href="https://nextjs.org/docs" p="6" m="4" borderWidth="1px" rounded="lg" flexBasis={['auto', '45%']}>
             <Heading as="h3" size="lg" mb="2">Documentation &rarr;</Heading>
             <Text fontSize="lg">Find in-depth information about Next.js features and API.</Text>
           </Box>
 
-          <Box as="a" href="https://nextjs.org/learn" p="6" m="4" borderWidth="1px" rounded="lg" flexBasis="45%">
+          <Box as="a" href="https://nextjs.org/learn" p="6" m="4" borderWidth="1px" rounded="lg" flexBasis={['auto', '45%']}>
             <Heading as="h3" size="lg" mb="2">Learn &rarr;</Heading>
             <Text fontSize="lg">Learn about Next.js in an interactive course with quizzes!</Text>
           </Box>
 
-          <Box as="a" href="https://github.com/vercel/next.js/tree/master/examples" p="6" m="4" borderWidth="1px" rounded="lg" flexBasis="45%">
+          <Box as="a" href="https://github.com/vercel/next.js/tree/master/examples" p="6" m="4" borderWidth="1px" rounded="lg" flexBasis={['auto', '45%']}>
             <Heading as="h3" size="lg" mb="2">Examples &rarr;</Heading>
             <Text fontSize="lg">Discover and deploy boilerplate example Next.js projects.</Text>
           </Box>
 
-          <Box as="a" href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app" p="6" m="4" borderWidth="1px" rounded="lg" flexBasis="45%">
+          <Box as="a" href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app" p="6" m="4" borderWidth="1px" rounded="lg" flexBasis={['auto', '45%']}>
             <Heading as="h3" size="lg" mb="2">Deploy &rarr;</Heading>
             <Text fontSize="lg">Instantly deploy your Next.js site to a public URL with Vercel.</Text>
           </Box>
 
-          <Box as="a" href="https://chakra-ui.com/" p="6" m="4" borderWidth="1px" rounded="lg" flexBasis="45%">
+          <Box as="a" href="https://chakra-ui.com/" p="6" m="4" borderWidth="1px" rounded="lg" flexBasis={['auto', '45%']}>
             <Heading as="h3" size="lg" mb="2">Chakra UI &rarr;</Heading>
             <Text fontSize="lg">Build accessible React apps & websites with speed.</Text>
           </Box>
