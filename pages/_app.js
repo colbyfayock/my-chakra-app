8538b3609cfac71b6ece60e36314edf9a189941b
colbyfayock <fayock@gmail.com>
2020-10-18 09:53:27 -0400

    Step 1: installing Chakra and configuring it in a React app

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,14 @@
+import { ThemeProvider, theme, CSSReset } from '@chakra-ui/core';
+
 import '../styles/globals.css'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <ThemeProvider theme={theme}>
+      <CSSReset />
+      <Component {...pageProps} />
+    </ThemeProvider>
+  )
 }
 
 export default MyApp
