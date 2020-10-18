a324f8cd1d4120027a7f4dbcb16f45980de5495a
colbyfayock <fayock@gmail.com>
2020-10-18 11:21:55 -0400

    Step 2: adding components to recreate Next.js template

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,6 @@
 import Head from 'next/head'
+import { Heading, Link, Text, Code, Flex, Box } from "@chakra-ui/core";
+
 import styles from '../styles/Home.module.css'
 
 export default function Home() {
@@ -10,44 +12,40 @@ export default function Home() {
       </Head>
 
       <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
+        <Heading as="h1" size="2xl" mb="2">
+          Welcome to <Link color="teal.500" href="https://nextjs.org">Next.js!</Link>
+        </Heading>
 
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
-        </p>
+        <Text fontSize="xl" mt="2">
+          Get started by editing <Code>pages/index.js</Code>
+        </Text>
 
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
+        <Flex flexWrap="wrap" alignItems="center" justifyContent="center" maxW="800px" mt="10">
+          <Box as="a" href="https://nextjs.org/docs" p="6" m="4" borderWidth="1px" rounded="lg" flexBasis="45%">
+            <Heading as="h3" size="lg" mb="2">Documentation &rarr;</Heading>
+            <Text fontSize="lg">Find in-depth information about Next.js features and API.</Text>
+          </Box>
 
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
+          <Box as="a" href="https://nextjs.org/learn" p="6" m="4" borderWidth="1px" rounded="lg" flexBasis="45%">
+            <Heading as="h3" size="lg" mb="2">Learn &rarr;</Heading>
+            <Text fontSize="lg">Learn about Next.js in an interactive course with quizzes!</Text>
+          </Box>
 
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
+          <Box as="a" href="https://github.com/vercel/next.js/tree/master/examples" p="6" m="4" borderWidth="1px" rounded="lg" flexBasis="45%">
+            <Heading as="h3" size="lg" mb="2">Examples &rarr;</Heading>
+            <Text fontSize="lg">Discover and deploy boilerplate example Next.js projects.</Text>
+          </Box>
 
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
+          <Box as="a" href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app" p="6" m="4" borderWidth="1px" rounded="lg" flexBasis="45%">
+            <Heading as="h3" size="lg" mb="2">Deploy &rarr;</Heading>
+            <Text fontSize="lg">Instantly deploy your Next.js site to a public URL with Vercel.</Text>
+          </Box>
+
+          <Box as="a" href="https://chakra-ui.com/" p="6" m="4" borderWidth="1px" rounded="lg" flexBasis="45%">
+            <Heading as="h3" size="lg" mb="2">Chakra UI &rarr;</Heading>
+            <Text fontSize="lg">Build accessible React apps & websites with speed.</Text>
+          </Box>
+        </Flex>
       </main>
 
       <footer className={styles.footer}>
