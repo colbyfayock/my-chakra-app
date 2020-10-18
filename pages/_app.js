b9d707ce3324207c25c2348934ca0c506402bd2f
colbyfayock <fayock@gmail.com>
2020-10-18 12:05:57 -0400

    Step 4: customizing the default Chakra theme

--- pages/_app.js
+++ pages/_app.js
@@ -2,9 +2,29 @@ import { ThemeProvider, theme, CSSReset } from '@chakra-ui/core';
 
 import '../styles/globals.css'
 
+const customTheme = {
+  ...theme,
+  colors: {
+    ...theme.colors,
+    purple: {
+      50: '#f5e9ff',
+      100: '#dac1f3',
+      200: '#c098e7',
+      300: '#a571dc',
+      400: '#8c48d0',
+      500: '#722fb7',
+      600: '#59238f',
+      700: '#3f1968',
+      800: '#260f40',
+      900: '#10031a',
+    },
+    spacejelly: '#692ba8'
+  }
+}
+
 function MyApp({ Component, pageProps }) {
   return (
-    <ThemeProvider theme={theme}>
+    <ThemeProvider theme={customTheme}>
       <CSSReset />
       <Component {...pageProps} />
     </ThemeProvider>
@@ -12,3 +32,4 @@ function MyApp({ Component, pageProps }) {
 }
 
 export default MyApp
+
--- pages/index.js
+++ pages/index.js
@@ -13,7 +13,7 @@ export default function Home() {
 
       <main className={styles.main}>
         <Heading as="h1" size="2xl" mb="2">
-          Welcome to <Link color="teal.500" href="https://nextjs.org">Next.js!</Link>
+          Welcome to <Link color="spacejelly" href="https://nextjs.org">Next.js!</Link>
         </Heading>
 
         <Text fontSize="xl" mt="2">
